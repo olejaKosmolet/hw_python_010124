8b74b7a4a9fab228957a608b90d88c64036eaef1
Ermolaev <olegermolaev45@gmail.com>
2023-12-28 16:11:43 +0300

    added functions

--- phone_book.py
+++ phone_book.py
@@ -29,17 +29,41 @@
 #     - реализация запроса пользователя; 
 #     - выход из программы;
 
-def interface():
-    print('Доступные запросы: \n'
-          '1. Добавить контакт.\n'
-          '2. Вывести на экран\n'
-          '3. Поиск контакта\n'
-          '4. Выход из программы')
-    
-    command = input('Выберите из списка номер команды: ')
-    
-    while command  not in ('1', '2', '3', '4'):
-        print("Некорректный ввод данных")
+def add_contact():
+    pass
+
+def print_info():
+    pass
+
+def search_contact():
+    pass
+
+
+
+
+
+def interface():    
+    command = '-1'
+    while command != '4':
+        print('Доступные запросы: \n'
+                '1. Добавить контакт.\n'
+                '2. Вывести на экран\n'
+                '3. Поиск контакта\n'
+                '4. Выход из программы')
+
         command = input('Выберите из списка номер команды: ')
 
+        while command  not in ('1', '2', '3', '4'):
+            print("Некорректный ввод данных")
+            command = input('Выберите из списка номер команды: ')
+
+        match command:
+            case '1':
+                add_contact()
+            case '2':
+                print_info()
+            case '3':
+                search_contact()
+            case '4':
+                print('Bye bye!')
     
